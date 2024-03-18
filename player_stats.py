a0df5030eb44dfbe674d233d4e00edb699d693b8
Benjamin Barker <benjamin99999@icloud.com>
2024-03-18 21:10:13 +0000

    fully working and debuged program

--- player_stats.py
+++ player_stats.py
@@ -4,7 +4,9 @@ import aiohttp
 import sqlite3
 from understat import Understat
 import re
+from datetime import date
 
+# Gets all of the stats from understat
 async def main():
   async with aiohttp.ClientSession() as session:
     understat = Understat(session)
@@ -13,6 +15,7 @@ async def main():
     )
     return all_players_stats
   
+# protects aganst SQL injections
 def clean(text):
     text = re.sub(r' ', '_zzzzzspacezzzzz_', text)
     text = re.sub(r'-', '_zzzzzdashzzzzz_', text)
@@ -21,15 +24,17 @@ def clean(text):
     text = re.sub(r'_zzzzzdashzzzzz_', '-', text)
     return text
 
+# Creates a table for a set position
 def create_table(sql_connection,table_name,position,order_by,direction):
   sql_cursor = sql_connection.cursor()
   sql_cursor.execute(f"CREATE TABLE {table_name} AS SELECT * FROM player_stats WHERE player_position LIKE '%{clean(position)}%' ORDER BY {clean(order_by)} {clean(direction)}")
-  sql_connection.commit()
+  #  # prints out the stats for the players in the table
   # length = sql_cursor.execute(f'SELECT COUNT(*) FROM {clean(table_name)}').fetchone()[0]
   # sorted_player_stats = sql_cursor.execute(f"SELECT player_name,player_position,player_score FROM {clean(table_name)}")
   # for i in range(0,length):
   #   print(sorted_player_stats.fetchone())
 
+# Allows users to select between a set of formation 3 templates
 def create_team(sql_connection,team_name):
   formation_complete = False
   while formation_complete == False:
@@ -59,6 +64,7 @@ def create_team(sql_connection,team_name):
       for i in range(0,number_of_forwards):
         add_player(sql_connection,'forward',team_name)
 
+# Allows the user to choose who they want to put in their team
 def add_player(sql_connection,table_name,team_name):
   sql_cursor = sql_connection.cursor()
   pick_complete = False
@@ -97,6 +103,75 @@ def add_player(sql_connection,table_name,team_name):
       else:
         print(f'{clean(player_chosen)} SELECTED')
         sql_cursor.execute(f'INSERT INTO {clean(team_name)} SELECT * FROM {clean(table_name)} WHERE player_name LIKE "{clean(player_chosen)}"')
+        sql_cursor.execute(f'UPDATE {team_name} SET player_position = "{clean(table_name)}" WHERE player_name LIKE "{clean(player_chosen)}"')
+
+# Allows the user to
+def player_selection(sql_connection,team_name):
+  sql_cursor = sql_connection.cursor()
+  pick_complete = False
+  while pick_complete == False:
+    swap_player = input('Which player do you want to swap: ')
+    swap_player = sql_cursor.execute(f'SELECT player_name FROM {team_name} WHERE player_name LIKE "%{swap_player}%"').fetchall()
+    if len(swap_player) == 0:
+      print('There is no player in this postition with this name. Make sure the name of the player you want is spelt correctly.')
+    elif len(swap_player) > 1:
+      print('There are multiple players with a similar names.')
+      for i in range(0,len(swap_player)):
+        print(f'Enter {i+1} for {swap_player[i][0]}.')
+      print('Enter (0) to select another player.')
+      player_number = -1
+      while player_number > len(swap_player) or player_number < 0:
+        try:
+          player_number = int(input('Please select the number corresponding to the player you want: '))
+        except ValueError:
+          pass
+      if player_number == 0:
+        pass
+      else:
+        swap_player = swap_player[player_number-1][0]
+        pick_complete = True
+    elif len(swap_player) == 1:
+      swap_player = swap_player[0][0]
+      pick_complete = True
+    else:
+      print('Unexpected error.')
+    if pick_complete == True:
+      print('')
+      individual_stats = sql_cursor.execute(f'SELECT * FROM {team_name} WHERE player_name = "{swap_player}"').fetchall()
+      print("Name".ljust(28), individual_stats[0][1])
+      print("Games".ljust(28), individual_stats[0][2])
+      print("Minutes".ljust(28), individual_stats[0][3])
+      print("Goals".ljust(28), individual_stats[0][4])
+      print("Expected Goals".ljust(28), f"{individual_stats[0][5]:.2f}")
+      print("Assists".ljust(28), individual_stats[0][6])
+      print("Expected assists".ljust(28), f"{individual_stats[0][7]:.2f}")
+      print("Shots".ljust(28), individual_stats[0][8])
+      print("Key Passes".ljust(28), individual_stats[0][9])
+      print("Yellow Cards".ljust(28), individual_stats[0][10])
+      print("Red Cards".ljust(28), individual_stats[0][11])
+      print("Position".ljust(28), individual_stats[0][12])
+      print("Team Name".ljust(28), individual_stats[0][13])
+      print("Non Penalty Goals".ljust(28), individual_stats[0][14])
+      print("Non Penalty Expected Goals".ljust(28), f"{individual_stats[0][15]:.2f}")
+      print("Expected Goals Chain".ljust(28), f"{individual_stats[0][16]:.2f}")
+      print("Expected Goals Build-up".ljust(28), f"{individual_stats[0][17]:.2f}")
+      print("Score".ljust(28), individual_stats[0][18])
+      confirmed_swap_player = ''
+      while confirmed_swap_player != 'y' and confirmed_swap_player != 'n':
+        confirmed_swap_player = input(f'\nEnter Y to confirm you want to choose {swap_player}. Enter N if you want to select a different player: ').lower()
+      if confirmed_swap_player == 'y':
+        return swap_player
+      else:
+        pick_complete = False
+
+    #   player_check = sql_cursor.execute(f'SELECT COUNT(*) FROM {clean(team_name)} WHERE player_name LIKE "{clean(swap_player)}"')
+    #   player_check = player_check.fetchall()[0][0]
+    #   if player_check != 0:
+    #     pick_complete = False
+    #     print(f'{clean(swap_player)} is already in your team. Please choose another player.')
+    #   else:
+    #     print(f'{clean(swap_player)} SELECTED')
+    #     sql_cursor.execute(f'INSERT INTO {clean(team_name)} SELECT * FROM {clean(table_name)} WHERE player_name LIKE "{clean(swap_player)}"')
 
 loop = asyncio.get_event_loop()
 all_players_stats = loop.run_until_complete(main())
@@ -139,9 +214,8 @@ for i in range(len(all_players_stats)):
   sql_cursor.execute("INSERT INTO player_stats VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", formatted_stats)
 sql_connection.commit()
 
-sql_cursor.execute("ALTER TABLE player_stats ADD player_score")
-sql_cursor.execute("UPDATE player_stats SET player_score = goals*7 + assists*10 + minutes/90 + yellow_cards*-2 + red_cards*-15 + non_penalty_goals + expected_goals_build_up*5")
-sql_cursor.execute("UPDATE player_stats SET player_score = round(player_score)")
+sql_cursor.execute("ALTER TABLE player_stats ADD player_score INTEGER")
+sql_cursor.execute("UPDATE player_stats SET player_score = cast(round(goals*7 + assists*10 + minutes/90 + yellow_cards*-2 + red_cards*-15 + non_penalty_goals + expected_goals_build_up*5,0) as integer)")
 sql_connection.commit()
 
 length = sql_cursor.execute(f'SELECT COUNT(*) FROM player_stats').fetchone()[0]
@@ -152,7 +226,7 @@ create_table(sql_connection,'defence','D','player_score','DESC')
 create_table(sql_connection,'midfield','M','player_score','DESC')
 create_table(sql_connection,'forward','F','player_score','DESC')
 
-sql_cursor.execute('CREATE TABLE IF NOT EXISTS user_info(team_name,password,team_score)')
+sql_cursor.execute('CREATE TABLE IF NOT EXISTS user_info(team_name,password,team_score,week)')
 user_info_pointer = sql_cursor.execute('SELECT * FROM user_info')
 user_info = user_info_pointer.fetchall()
 print(user_info)
@@ -169,7 +243,6 @@ for i in range(len(user_info)):
                       key_passes = player_stats.key_passes, \
                       yellow_cards = player_stats.yellow_cards, \
                       red_cards = player_stats.red_cards, \
-                      player_position = player_stats.player_position, \
                       team_name = player_stats.team_name, \
                       non_penalty_goals = player_stats.non_penalty_goals, \
                       non_penalty_expected_goals = player_stats.non_penalty_expected_goals, \
@@ -179,9 +252,9 @@ for i in range(len(user_info)):
                       FROM player_stats WHERE {user_info[i][0]}.player_name = player_stats.player_name')                                                  # update individual player scores
   sql_cursor.execute(f'UPDATE user_info SET team_score = SUM(player_score) FROM {clean(user_info[i][0])} WHERE user_info.team_name = "{clean(user_info[i][0])}"')
 
-print(sql_cursor.execute(f'SELECT * FROM player_stats WHERE player_name IN (SELECT player_name FROM {clean(user_info[0][0])}) ORDER BY goals DESC').fetchall())
-print('/n/n/n')
-print(sql_cursor.execute(f'SELECT * FROM {clean(user_info[0][0])} ORDER BY goals DESC').fetchall())
+# print(sql_cursor.execute(f'SELECT * FROM player_stats WHERE player_name IN (SELECT player_name FROM {clean(user_info[0][0])}) ORDER BY goals DESC').fetchall())
+# print('/n/n/n')
+# print(sql_cursor.execute(f'SELECT * FROM {clean(user_info[0][0])} ORDER BY goals DESC').fetchall())
 
 
 user_info_pointer = sql_cursor.execute('SELECT * FROM user_info')
@@ -212,7 +285,7 @@ while login_complete == False:
       elif len(password) < 5:
         print("Your password is too short make sure it is a minimum of 5 charaters.\n")
       else:
-        sql_cursor.execute("INSERT INTO user_info VALUES(?,?,?)", (team_name,password,0))
+        sql_cursor.execute("INSERT INTO user_info VALUES(?,?,?,?)", (team_name,password,0,0))
         create_table(sql_connection,team_name,'T','player_score','DESC')
         create_team(sql_connection,team_name)
         sql_connection.commit()
@@ -224,9 +297,41 @@ while login_complete == False:
   elif team_name_check[0][0] != password:
     print('Make sure to enter your password correctly. Be careful of capitals.')
 
-print(f'Your team has the following players:')
-team = sql_cursor.execute(f'SELECT player_name FROM {team_name}')
-for i in range(0,11):
-  print(team.fetchone()[0])
+positions = ['goalkeeper','defence','midfield','forward']
+print(f'Your team has the following players:\n')
+print(f'Name                         Position    Score')
+for i in range(0,len(positions)):
+  team = sql_cursor.execute(f'SELECT player_name,player_position,player_score FROM {team_name} WHERE player_position LIKE "{positions[i]}"').fetchall()
+  for i in range(0,len(team)):
+    print(team[i][0].ljust(28), team[i][1].ljust(12),team[i][2])
 team_score = sql_cursor.execute(f'SELECT SUM(player_score) FROM {team_name}')
 print(f'Your score is {team_score.fetchall()[0][0]}')
+
+week = date.today().isocalendar()[1]
+last_transfer = sql_cursor.execute('SELECT week FROM user_info WHERE team_name = ?',(team_name,)).fetchall()[0][0]
+# if week != last_transfer:
+if 1 == 1:
+  transfer = 'z'
+  while transfer != 'y' and transfer != 'n':
+    transfer = input("If you want to make a transfer (Enter Y). If you don't want to make a transfer (Enter N). ").lower()
+  if transfer == 'y':
+    print('Choose the player you want to swap out.')
+    swap_out_player = player_selection(sql_connection,team_name)
+    swap_complete = False
+    while swap_complete == False:
+      print('Choose the player you want to replace them with.')
+      position = sql_cursor.execute(f'SELECT player_position FROM {team_name} WHERE player_name = "{swap_out_player}"').fetchall()[0][0]
+      swap_in_player = player_selection(sql_connection,position)
+      player_check = sql_cursor.execute(f'SELECT COUNT(*) FROM {clean(team_name)} WHERE player_name LIKE "{clean(swap_in_player)}"')
+      player_check = player_check.fetchall()[0][0]
+      if player_check != 0:
+        print(f'{clean(swap_in_player)} is already in your team. Please choose another player.')
+      else:
+        print(f'{clean(swap_in_player)} has been transfered in for {swap_out_player}')
+        sql_cursor.execute(f'DELETE FROM {clean(team_name)} WHERE player_name = "{clean(swap_out_player)}"')
+        sql_cursor.execute(f'INSERT INTO {clean(team_name)} SELECT * FROM player_stats WHERE player_name = "{clean(swap_in_player)}"')
+        sql_cursor.execute(f'UPDATE {clean(team_name)} SET player_position = "{position}" WHERE player_name = "{clean(swap_in_player)}"')
+        sql_cursor.execute(f'UPDATE user_info SET week = {int(week)} WHERE team_name = "{clean(team_name)}"')
+        sql_connection.commit()
+        swap_complete = True
+date.today().isocalendar()[1]
--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-import re
+# import re
 
-def clean(text):
-    text = re.sub(r' ', '_zzzzzspacezzzzz_', text)
-    text = re.sub(r'-', '_zzzzzdashzzzzz_', text)
-    text = re.sub(r'[\W]', '', text)
-    text = re.sub(r'_zzzzzspacezzzzz_', ' ', text)
-    text = re.sub(r'_zzzzzdashzzzzz_', '-', text)
-    return text
+# def clean(text):
+#     text = re.sub(r' ', '_zzzzzspacezzzzz_', text)
+#     text = re.sub(r'-', '_zzzzzdashzzzzz_', text)
+#     text = re.sub(r'[\W]', '', text)
+#     text = re.sub(r'_zzzzzspacezzzzz_', ' ', text)
+#     text = re.sub(r'_zzzzzdashzzzzz_', '-', text)
+#     return text
 
-dirty_text = input("Give me some text to clean: ")
-print(clean(dirty_text))
+# dirty_text = input("Give me some text to clean: ")
+# print(clean(dirty_text))
+
+import sqlite3
+
+sql_connection = sqlite3.connect('sql_database.db')
+sql_cursor = sql_connection.cursor()
+print(sql_cursor.execute("SELECT cast(210.0 as integer)").fetchall())
+print(sql_cursor.execute("SELECT cast(209.4 as integer)").fetchall())
+print(sql_cursor.execute("SELECT cast(round(209.5,0) as integer)").fetchall())
+print(sql_cursor.execute("SELECT cast(209.0 as integer)").fetchall())
+print(sql_cursor.execute("SELECT cast(210 as integer)").fetchall())
